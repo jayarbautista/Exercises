6d8f1ec5456ccc9333c75744e660fdcd29cecc77
jayarbautista <j.bautista@klab.com>
2014-08-21 16:39:50 +0800

    Added the implementation of Toast

--- SimpleButtons/src/com/android/exercises/simplebuttons/SimpleButtonActivity.java
+++ SimpleButtons/src/com/android/exercises/simplebuttons/SimpleButtonActivity.java
@@ -7,9 +7,13 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SimpleButtonActivity extends ActionBarActivity {
 	private static final String TAG = "SimpleButtonActivity";
+	
+	private String mMessage;
+	
 	TextView mText;
 	Button mOkButton;
 	Button mCancelButton;
@@ -18,28 +22,33 @@ public class SimpleButtonActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_simple_button);
+
 		Log.d(TAG, "Find view elements");
-		mText = (TextView) findViewById(R.id.tvOut);
-		mOkButton = (Button) findViewById(R.id.ok_button);
-		mCancelButton = (Button) findViewById(R.id.cancel_button);
+		mText = (TextView)findViewById(R.id.tvOut);
+		mOkButton = (Button)findViewById(R.id.ok_button);
+		mCancelButton = (Button)findViewById(R.id.cancel_button);
+
 		Log.d(TAG, "Set listener to buttons");
 		mOkButton.setOnClickListener(mOnClick);
 		mCancelButton.setOnClickListener(mOnClick);
 	}
-
+	
 	OnClickListener mOnClick = new OnClickListener() {
 		@Override
 		public void onClick(View v) {
-			switch (v.getId()) {
-			case (R.id.ok_button):
+			switch(v.getId()) {
+			case(R.id.ok_button):
 				Log.d(TAG, "Clicked: OK button");
 				mText.setText("Button OK was clicked.");
 				break;
-			case (R.id.cancel_button):
+			case(R.id.cancel_button):
 				Log.d(TAG, "Clicked: Cancel button");
 				mText.setText("Button Cancel was clicked.");
 				break;
 			}
+
+			mMessage = (String) mText.getText();
+			Toast.makeText(getApplicationContext(), mMessage, Toast.LENGTH_SHORT).show();
 		}
 	};
 }
