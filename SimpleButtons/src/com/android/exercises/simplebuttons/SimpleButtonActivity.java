58e1e097b991827018beb07725bb9183974a312e
jayarbautista <j.bautista@klab.com>
2014-08-21 16:38:44 +0800

    Added the implementation of android.util.Log

--- SimpleButtons/src/com/android/exercises/simplebuttons/SimpleButtonActivity.java
+++ SimpleButtons/src/com/android/exercises/simplebuttons/SimpleButtonActivity.java
@@ -2,12 +2,14 @@ package com.android.exercises.simplebuttons;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class SimpleButtonActivity extends ActionBarActivity {
+	private static final String TAG = "SimpleButtonActivity";
 	TextView mText;
 	Button mOkButton;
 	Button mCancelButton;
@@ -16,9 +18,11 @@ public class SimpleButtonActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_simple_button);
+		Log.d(TAG, "Find view elements");
 		mText = (TextView) findViewById(R.id.tvOut);
 		mOkButton = (Button) findViewById(R.id.ok_button);
 		mCancelButton = (Button) findViewById(R.id.cancel_button);
+		Log.d(TAG, "Set listener to buttons");
 		mOkButton.setOnClickListener(mOnClick);
 		mCancelButton.setOnClickListener(mOnClick);
 	}
@@ -28,9 +32,11 @@ public class SimpleButtonActivity extends ActionBarActivity {
 		public void onClick(View v) {
 			switch (v.getId()) {
 			case (R.id.ok_button):
+				Log.d(TAG, "Clicked: OK button");
 				mText.setText("Button OK was clicked.");
 				break;
 			case (R.id.cancel_button):
+				Log.d(TAG, "Clicked: Cancel button");
 				mText.setText("Button Cancel was clicked.");
 				break;
 			}
